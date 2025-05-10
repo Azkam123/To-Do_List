8c4a8b9c9121b06bad9ba4c5ae09cf3d08480322
moh hasan <mohhasan9998@gmail.com>
2025-05-10 17:21:38 +0700

    menambhkan mengimplementasikan about_screen.dart

---
+++ lib/screens/about_screen.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class AboutScreen extends StatelessWidget{
+  @override
+  Widget build (BuildContext context){
+    return Scaffold(
+      appBar: AppBar(title: Text("Tentang Aplikasi")),
+     body: Center(
+      child: Padding(
+        padding: const EdgeInsets.all(20),
+        child: Text(
+          "Aplikasi To-Do List Versi Flutter\n Dibuat oleh Moh Hasan Mahasiswa IT",
+          textAlign: TextAlign.center,
+          style: Theme.of(context).textTheme.titleLarge,
+        ),
+        
+      ),
+     ), 
+    );
+  }
+}
